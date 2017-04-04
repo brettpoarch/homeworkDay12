9490f0a246151ab9c9da94aa4cf7c60f90481942
brett poarch <brett.poarch@gmail.com>
2017-04-03 22:31:58 -0700

    all data on page

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 $(document).ready(function(){
 	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(formData){
 		console.log(formData)
-		
-		var imgs = ''
+
+		var info = ''
 
 		formData.results.forEach(function(value){
 			value.Images.forEach(function(value){
 				if(value.rank === 1){
-					console.log(value)
-					imgs += `<img src=${value.url_170x135} />`
+					info += `<div class='desc'><img src=${value.url_170x135} />`
 				}
-
 			})
-				$("#app").html(`<div>${imgs}</div>`);
-
+			info += `<p>${value.title}</p> <p>${value.Shop.shop_name + ' $' + value.price }</p></div>`
 		})
+		$("#app").html(`${info}`);
 	})
 })
 
